517570f5bdda0d3d37784a8bee3313a42d970144
조종운 <woon@jojong-un-ui-MacBookPro.local>
2021-01-16 21:47:19 +0900

    bit-cartoon

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const cartoonApi = axios.create({
-  baseURL: "http://ec2-3-133-137-52.us-east-2.compute.amazonaws.com",
-  // refreshToken을 Cookie로 주고받기 위해..
+  baseURL: "https://ec2-3-133-137-52.us-east-2.compute.amazonaws.com",
+  withCredentials: false, // refreshToken을 Cookie로 주고받기 위해..
 });
 
 export const loginApi = {
@@ -42,7 +42,7 @@ export const imageApi = {
     return cartoonApi({
       method: "post",
       url:
-        "http://ec2-3-133-137-52.us-east-2.compute.amazonaws.com/cartoon/transition",
+        "https://ec2-3-133-137-52.us-east-2.compute.amazonaws.com/cartoon/transition",
       data: form,
       headers: { "Content-Type": "multipart/form-data" },
     });
