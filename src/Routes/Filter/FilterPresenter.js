88c1755145e93cb05ba21ceac9ccca606b578f8e
luckycontrol <beansu1@naver.com>
2021-02-09 06:56:49 +0900

    bit-cartoon

--- src/Routes/Filter/FilterPresenter.js
+++ src/Routes/Filter/FilterPresenter.js
@@ -3,11 +3,11 @@ import Plot from "react-plotly.js";
 import Plotly from "plotly.js";
 
 const FilterPresenter = () => {
-  
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/3d-scatter.csv', (err, rows) => {
-      rows.map((row) => console.log(row.x1))
+    Plotly.d3.csv('https://raw.githubusercontent.com/luckycontrol/bit-cartoon/master/public/colors/5Centimater_crop_BGR.csv', (err, rows) => {
+      console.log(rows)
     })
   }, []);
 
