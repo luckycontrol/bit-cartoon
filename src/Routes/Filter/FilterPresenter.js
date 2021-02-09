6c9e4071e39f8b853a3af9f03c8b66a962306438
luckycontrol <beansu1@naver.com>
2021-02-09 09:18:13 +0900

    bit-cartoon

--- src/Routes/Filter/FilterPresenter.js
+++ src/Routes/Filter/FilterPresenter.js
@@ -3,16 +3,43 @@ import Plot from "react-plotly.js";
 import Plotly from "plotly.js";
 
 const FilterPresenter = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
 
   useEffect(() => {
+    let data = {
+      x: [],
+      y: [],
+      z: [],
+    }
     Plotly.d3.csv('https://raw.githubusercontent.com/luckycontrol/bit-cartoon/master/public/colors/5Centimater_crop_BGR.csv', (err, rows) => {
-      console.log(rows)
-    })
+      function unpack(rows, key) {
+        return rows.map((row) => {
+          return row[key];
+        });
+      }
+
+      data.x = unpack(rows, 'x');
+      data.y = unpack(rows, 'y');
+      data.z = unpack(rows, 'z');
+
+      setData(data);
+    });
   }, []);
 
   return (
-    <></>
+    <>
+      <Plot 
+        data={[
+          {
+            x: data.x, y: data.y, z: data.z,
+            type: 'scatter3d',
+            mode: 'markers',
+            marker: {color: `rgba(${data.x}, ${data.y}, ${data.z}, 0.7)`}
+          }
+        ]}
+        layout={ {width: 1000, height: 1000} }
+      />
+    </>
   );
 };
 
